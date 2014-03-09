4d37934353c74fd3bf61671932f9d1579255f4ed
Jose Lee <joselee@conmio.com>
2014-03-10 01:15:45 +0200

    Add probe. Give it animations. Add collider to it. Attach probe script.

--- Assets/Scripts/Game objects/units/Probe.cs
+++ Assets/Scripts/Game objects/units/Probe.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeavyGuy : Unit {
+public class Probe : Unit {
+
 	void Start()
 	{
-		this.SelectAnimations = new string[] { "shoot_01", "shoot_02", "shoot_03", "emo_1" };		
-		this.DefaultAnimation = "idle_1";
+		this.SelectAnimations = new string[] { "Probe_Idle" };		
+		this.DefaultAnimation = "Probe_Idle";
 		base.Start();
 		NavMeshAgent agent = (NavMeshAgent)gameObject.GetComponent<NavMeshAgent>();
 		agent.angularSpeed = 200;
 		agent.speed = 10;
 		agent.acceleration = 8;
 		
-		this.animation["walk"].speed = 3f;
-		this.animation["emo_1"].speed = 2f;
+		//this.animation["walk"].speed = 3f;
 	}
 }
